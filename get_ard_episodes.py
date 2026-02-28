0bde082dda909429972327392017fc33c6aa7391
Tobias Reichenbach <toto.rei0210@gmail.com>
2026-02-28 13:33:56 +0100

    get_ard_episodes: add progress bar

--- get_ard_episodes.py
+++ get_ard_episodes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import requests, json, time
+import requests, json
+from tqdm import tqdm
 
 PAGE_SIZE = 200
 ASSET_ID = "Y3JpZDovL3N3ci5kZS8yMjUwOTg2"
@@ -13,21 +14,25 @@ def append_episodes(episode_list):
         episodes.append(episode)
 
 def get_episode_list():
+    print("fetching ARD episode list...")
     r = requests.get(f"https://api.ardmediathek.de/page-gateway/widgets/ard/asset/{ASSET_ID}?pageNumber=0&pageSize={PAGE_SIZE}")
     r_json = r.json()
     total_elements = r_json["pagination"]["totalElements"]
 
     append_episodes(r_json["teasers"])
 
-    for page_number in range(1, total_elements // PAGE_SIZE + 1):
+    for page_number in tqdm(range(1, total_elements // PAGE_SIZE + 1)):
         r = requests.get(f"https://api.ardmediathek.de/page-gateway/widgets/ard/asset/{ASSET_ID}?pageNumber={page_number}&pageSize={PAGE_SIZE}")
         append_episodes(r.json()["teasers"])
 
     with open("./json/ard_episodes_original.json", "w") as f:
         json.dump(episodes, f)
+    
+    print("Done!")
 
 def get_episode_details():
-    for episode in episodes:
+    print("fetching epidsode details...")
+    for episode in tqdm(episodes):
         r = requests.get(f"https://api.ardmediathek.de/page-gateway/pages/ard/item/{episode["id"]}?embedded=false&mcV6=true")
         new_episode_list.append({
             "id": episode["id"],
@@ -37,11 +42,10 @@ def get_episode_details():
             "coreAssetType": episode["coreAssetType"],
             "synopsis": r.json()["widgets"][0]["synopsis"],
         })
-        print(r.json()["widgets"][0]["synopsis"])
-        print()
 
     with open("./json/ard_episode_list.json", "w") as f:
         json.dump(new_episode_list, f)
+    print("Done!")
 
 get_episode_list()
 get_episode_details()
